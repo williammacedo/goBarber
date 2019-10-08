2e6b041d2de6435419f32e1160644c1d771b3f21
William <macedo.franca@gmail.com>
2019-10-08 10:00:27 -0300

    Add middleware to permit access to specific routes, only to logged users. Route to update users created.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authHeader from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authHeader);
+
+routes.put('/users', UserController.update);
 
 export default routes;
