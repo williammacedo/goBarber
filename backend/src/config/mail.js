abfaa199413cc6f8c929447817d9e3ce33b46160
William <macedo.franca@gmail.com>
2019-10-10 13:40:50 -0300

    Send mails to canceled appointments

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '84cca2e764b246',
+    pass: 'b69cfc310dc5ed',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
