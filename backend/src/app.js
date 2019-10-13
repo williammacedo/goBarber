daa95eba2854475a66ccbbefd1a0f0de34ea050f
williammacedo <Will.9455426>
2019-10-13 00:16:04 -0300

    Configured environment variables and configured exception handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
+import 'dotenv/config';
+
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 
+import sentryConfig from './config/sentry';
 import routes from './routes';
 
 import './database';
@@ -10,8 +16,12 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
+    this.server.use(Sentry.Handlers.requestHandler());
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -25,6 +35,20 @@ class App {
 
   routes() {
     this.server.use(routes);
+
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error.' });
+    });
   }
 }
 
